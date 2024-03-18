e23a3b64c6b8c69810d9dbc2151b08d36993b4ff
Dcardenas <126295398+1101622696@users.noreply.github.com>
2024-03-18 11:40:28 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,30 +22,29 @@
 
 
 <div class="buscador">
-  <input type="text" placeholder="Busca por numero" id="buscadorinput" v-model.trim="buscador">
+  <input type="text" placeholder="Busca un pokemon" id="buscadorinput" v-model.trim="buscador">
 <button id="boton" @click="traer()"><img id="logito" src="https://codeboost.com.br/projetos/pokeapi/img/img-pokeball-02.png"></button>
   </div>
   
     <div class="card">
-      
-      <div  class="boletacomprada">
-          <div class="texto-nombrep">{{ pokemon.name }}</div>
-        <div class="datop">
-          <div class="texto-nombre">Peso</div>
-          <div class="texto-telefono">{{ pokemon.weight }}</div>
+        <div class="imagenes">
+          <div class="imgprincipal">
+      <img id="imgprincipal" :src="pokemon.sprites.front_shiny" alt="" v-if="pokemon.sprites">
+    </div>
+      <img id="imgdetras":src="pokemon.sprites.other.showdown.front_shiny" alt="" v-if="pokemon.sprites">
+      <img id="imgdetras":src="pokemon.sprites.other.showdown.back_shiny" alt="" v-if="pokemon.sprites">
+    </div>
+    <div  class="datos" v-if="showcard">
+      <div class="nombre">{{ pokemon.name }}</div>
+      <div class="tipo" v-for="(e, i) in pokemon.types" :key="i">Tipo: {{ e.type.name }} </div>
+          <div class="peso">Peso: {{ pokemon.weight }}</div>
         </div>
-      <img :src="pokemon.sprites.front_shiny" alt="" v-if="pokemon.sprites">
-      <img :src="pokemon.sprites.other.showdown.front_shiny" alt="" v-if="pokemon.sprites">
-      <img :src="pokemon.sprites.other.showdown.back_shiny" alt="" v-if="pokemon.sprites">
-        <h4 v-for="(e, i) in pokemon.types" :key="i">Tipo: {{ e.type.name }} </h4>
-      </div>
-
       <section class="linear-progress-section">
         <div class="linear-progress" v-for="(e, i) in pokemon.stats" :key="i">
-          <label :for="'ID' + i" class="stat-name text-xs font-semibold text-black">{{ e.stat.name }}</label>
-          <q-linear-progress :id="'ID' + i" :value="progressValue(e.base_stat)" :buffer="bufferValue(e.base_stat + 10)" rounded size="10px" color="blue">
+          <label :for="'ID' + i" rounded size="13px" color="purple" class="stat-name text-xs font-semibold text-white" >{{ e.stat.name }}</label>
+          <q-linear-progress :id="'ID' + i" :value="progressValue(e.base_stat)" :buffer="bufferValue(e.base_stat + 10)" rounded size="15px" color="purple">
             <div class="absolute-full flex flex-center">
-              <q-badge color="white" text-color="accent" :label="e.base_stat" />
+              <q-badge color="black" text-color="white" :label="e.base_stat" />
             </div>
           </q-linear-progress>
         </div>
@@ -75,6 +74,7 @@ import axios from "axios"
 
 let pokemon = ref([])
 let buscador = ref([])
+let showcard = ref(false);
 
 
 
@@ -86,6 +86,8 @@ async function traer() {
   } catch (error) {
     console.log(error);
   } 
+  showcard.value=true
+
 }
 const progressValue=(baseStat)=>`0.${baseStat}`
 const bufferValue=(baseStat)=>`0.${baseStat+10}`
@@ -96,28 +98,27 @@ const bufferValue=(baseStat)=>`0.${baseStat+10}`
 
 
 .card{
-  width: 70vw;
+  width: 80vw;
   height: 60vh;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  border: 1px solid rgb(255, 255, 255); 
+  box-sizing: border-box;
 }
 .buscador {
   margin-left: 20%;
   width: 17vw;
-  height:7vh;
+  height:8vh;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: cadetblue;
   box-shadow: 4px 8px 16px rgba(0, 0, 0, 0.5);
-  margin-top:-20%
+  margin-top:-30%
 
 }
 
@@ -139,7 +140,10 @@ const bufferValue=(baseStat)=>`0.${baseStat+10}`
   align-items: center;
   background-color: cadetblue;
 }
-
+#boton:hover{
+  border:none;
+  outline:none
+}
 #buscadorinput{
   width:85%;
   height:90%;
@@ -150,4 +154,68 @@ const bufferValue=(baseStat)=>`0.${baseStat+10}`
   padding-left: 3%;
 
 }
+
+.datos{
+  width:50vh;
+  height:30vh;
+  display:flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  justify-content: space-between;
+  color:white;
+  }
+.tipo{
+  font-size: 20px;
+
+}
+.nombre{
+color:purple;
+font-weight: 700;
+font-size:50px;
+backdrop-filter: blur(55px);
+text-transform:uppercase;
+font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+.peso{
+text-decoration: underline;
+font-size: 15px;
+}
+.imagenes{
+  width:50vh;
+  height:100%;
+}
+section{
+  width:60vh;
+  height:100%;
+  box-sizing: border-box;
+  top: 5%;
+  color:white
+}
+.imgprincipal{
+  width:100%;
+  height:60%;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#imgprincipal{
+  margin-top: -22%;
+  aspect-ratio: 16/9;
+  width:400px;
+  height:250px;
+  filter: drop-shadow(
+    0 0 10px rgb(0, 0, 0, .8)
+  )
+}
+
+#imgdetras{
+  width:40%;
+  height:30%;
+}
+.linear-progress{
+padding: 2%;
+}
+
 </style>
+
